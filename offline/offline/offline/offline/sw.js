e0421d93e95aac4311d42121d9eb9bba3b5a8874
Yosra Elkady <yosraelkady1@hotmail.com>
2026-02-10 09:56:28 +0400

    Create sw.js

---
+++ offline/offline/offline/offline/sw.js
@@ -0,0 +1,14 @@
+const CACHE = "monofer-offline-v1";
+const ASSETS = ["./index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
